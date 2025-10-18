4d66f06fdf98beb4062dea2c06cf0660ae2e18d8
Ian Jure Macalisang <ianjure.data@gmail.com>
2025-10-19 07:24:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -211,6 +211,7 @@ with col1:
             ),
             name="Forecasted Cases",
             highlight_function=lambda x: {'weight': 3, 'color': 'green'},
+            zoom_on_click=True
         ).add_to(map)
         
         # ADD BARANGAY NAME LAYER
@@ -358,6 +359,7 @@ with col2:
     
     styled_table = table_df.style.applymap(color_forecast, subset=['Risk Level'])
     st.dataframe(styled_table, width='stretch', height=380)
+
 
 
 
