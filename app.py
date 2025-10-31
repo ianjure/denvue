688a58bee51f6025a8fa2c9db7fa272b057e4ec7
Ian Jure Macalisang <ianjure.data@gmail.com>
2025-10-31 23:16:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ float_init()
 padding = """
 <style>
 .block-container {
-    padding-top: -5rem;
+    padding-top: 0rem;
     padding-bottom: 2.5rem;
 }
 </style>
@@ -416,6 +416,7 @@ with button_container:
     
 button_css = float_css_helper(width="3rem", height="3rem", right="0.8rem", top="0.6rem", transition=0)
 button_container.float(button_css)
+
 
 
 
