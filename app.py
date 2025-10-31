0401ceab16d5f6010bec602fbff5164f4faa85cf
Ian Jure Macalisang <ianjure.data@gmail.com>
2025-10-31 23:11:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -97,9 +97,10 @@ st.markdown(header_action, unsafe_allow_html=True)
 # [STREAMLIT] BOTTOM ALIGN CONTENT
 bottom_align = """
 <style>
-[class="stVerticalBlock st-emotion-cache-wfksaw e1wguzas3"] {
+.stColumn.st-emotion-cache-1wpb1x8.e1wguzas2 > .stVerticalBlock.st-emotion-cache-wfksaw.e1wguzas3 {
     display: flex;
     justify-content: flex-end;
+    align-items: flex-end;
 }
 </style>
 """
@@ -415,6 +416,7 @@ with button_container:
     
 button_css = float_css_helper(width="3rem", height="3rem", right="0.8rem", top="0.6rem", transition=0)
 button_container.float(button_css)
+
 
 
 
